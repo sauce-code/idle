145cbcca1362836008f7f7a9d57610b05d565cb0
Torben <sauce-code@users.noreply.github.com>
2023-08-29 16:30:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-import { initTableBuildings, updatePrices, startLoop, readCookie, writeCookie, clickThing, deleteCookie, reset, loopAutosave, rules, data, setTableVisibility } from './modules/game.js';
+import { init } from './modules/game.js';
 
-initTableBuildings();
-updatePrices();
-startLoop();
-readCookie();
-window.addEventListener("beforeunload", function () { writeCookie() });
-setInterval(loopAutosave, rules.saveInterval);
-document.getElementById("buttonSave").onclick = writeCookie;
-document.getElementById("buttonLoad").onclick = readCookie;
-document.getElementById("buttonDelete").onclick = deleteCookie;
-document.getElementById("buttonReset").onclick = reset;
-document.getElementById("clickImage").onclick = clickThing;
-document.getElementById("tableBuildingsRow" + 0)
-setTableVisibility();
+init()
